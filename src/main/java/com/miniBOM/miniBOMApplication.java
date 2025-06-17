ab71d25e0739d93371dcbd63f25c2e79f2776519
scut-wjr <202230482308@mail.scut.edu.cn>
2025-06-17 15:07:26 +0800

    777

---
+++ src/main/java/com/miniBOM/miniBOMApplication.java
@@ -0,0 +1,9 @@
+package com.miniBOM;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class miniBOMApplication {
+    public static void main(String[] args) {
+    }
+}
