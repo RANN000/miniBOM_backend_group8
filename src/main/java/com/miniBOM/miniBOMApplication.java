870e832de8d17d04c06156728a5053ec09064a0d
cjpcp <202230484272@mail.scut.edu.cn>
2025-06-18 10:25:36 +0800

    删除华为云依赖，解决依赖冲突问题，配置启动类（目前可以运行，未测试接口）

--- src/main/java/com/miniBOM/miniBOMApplication.java
+++ src/main/java/com/miniBOM/miniBOMApplication.java
@@ -1,9 +1,11 @@
 package com.miniBOM;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class miniBOMApplication {
     public static void main(String[] args) {
+        SpringApplication.run(miniBOMApplication.class, args);
     }
 }
