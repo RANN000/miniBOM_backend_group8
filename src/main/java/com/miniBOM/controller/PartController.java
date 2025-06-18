584b4fc2133372fd0bf7e96fb39139048c0dc1f5
cjpcp <202230484272@mail.scut.edu.cn>
2025-06-19 03:17:59 +0800

    对part封装

--- src/main/java/com/miniBOM/controller/PartController.java
+++ src/main/java/com/miniBOM/controller/PartController.java
@@ -15,21 +15,45 @@ public class PartController {
     @Autowired
     private PartService partService;
 
-    @PostMapping
+    @PostMapping("/add")
     public Result add(@RequestBody Part part) {
         partService.add(part);
         return Result.success();
     }
+    @PostMapping("/find")
+    public Result find(@RequestBody Part part) {
+        partService.find(part);
+        return Result.success();
+    }
+    @PostMapping("/listAllVersion")
+    public Result listAllVersion(@RequestBody Part part) {
+        partService.listAllVersion(part);
+        return Result.success();
+    }
 
     @GetMapping
-    public Result<List<Attribute>> list(String searchKey){
+    public Result<List<Part>> list(String searchKey){
         List<Part> at =partService.list(searchKey);
         return Result.success(at);
     }
 
+
     @PutMapping
     public Result update(@RequestBody Part part) {
         partService.update(part);
+        return Result.success();
+    }
+
+
+    @PostMapping("/checkOut")
+    public Result checkOut(@RequestBody Part part){
+        partService.checkOut(part);
+        return Result.success();
+    }
+
+    @PostMapping("/checkIn")
+    public Result checkIn(@RequestBody Part part){
+        partService.checkIn(part);
         return Result.success();
     }
 
--- src/main/java/com/miniBOM/service/PartService.java
+++ src/main/java/com/miniBOM/service/PartService.java
@@ -9,9 +9,21 @@ import java.util.List;
 public interface PartService {
     void add(Part part);
 
-    List<Part> list(String searchKey);
+    Part find(Part part);
+
+    List<Part> listAllVersion(Part part);
 
     void update(Part part);
 
+    void checkOut(Part part);
+
+    void checkIn(Part part);
+
     void delete(Part part);
+
+    List<Part> list(String searchKey);
+
+
+
+
 }
