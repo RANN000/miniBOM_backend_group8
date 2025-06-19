73e6e1139cc7a017a65412009da1ba1820946722
RANN000 <1170883985@qq.com>
2025-06-19 19:22:03 +0800

    Result类修改

--- src/main/java/com/miniBOM/pojo/Result.java
+++ src/main/java/com/miniBOM/pojo/Result.java
@@ -10,28 +10,29 @@ import java.util.List;
 @AllArgsConstructor
 @Data
 public class Result <T> {
+    private int code = 0;
     private String msg;
     public T data;
     public T error_data;
 
     public static Result error(String msg){
-        return new Result(msg,null,null);
+        return new Result(1,msg,null,null);
     }
 
     public static <E> Result<E> error(String msg, E error_data){
-        return new Result(msg,null,error_data);
+        return new Result(1,msg,null,error_data);
     }
 
     public static Result success(){
-        return new Result("SUCCESS",null,null);
+        return new Result(0,"SUCCESS",null,null);
     }
 
     public static <E> Result<E> success(E data){
-        return new Result("SUCCESS",data,null);
+        return new Result(0,"SUCCESS",data,null);
     }
 
     //其他逻辑错误
     public static Result otherError(String msg){
-        return new Result(msg,null,null);
+        return new Result(2,msg,null,null);
     }
 }
