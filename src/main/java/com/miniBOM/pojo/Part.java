0056e51220c4f2a0f7a95d8c7f993ed263cf0c3c
cjpcp <202230484272@mail.scut.edu.cn>
2025-06-19 09:39:48 +0800

    包装part必要参数

--- src/main/java/com/miniBOM/pojo/Part.java
+++ src/main/java/com/miniBOM/pojo/Part.java
@@ -1,4 +1,34 @@
 package com.miniBOM.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Part {
+    //add
+    String source;
+    String partType;
+    master master;
+    branch branch;
+    String applicationId;
+
+    //get
+    String id;//版本id
+
+    //listAllVersion,checkOut,checkIn,delete
+    String masterId;
+
+    //update
+    //版本id，同get
+    //修改属性，同add
+    String modifier;
+
+
+
 }
