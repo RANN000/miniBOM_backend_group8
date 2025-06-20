8c7dcf8386bab3229a876120337d4957e162f537
RANN000 <1170883985@qq.com>
2025-06-20 15:39:34 +0800

    Result类修改

--- src/main/java/com/miniBOM/pojo/Result.java
+++ src/main/java/com/miniBOM/pojo/Result.java
@@ -1,5 +1,6 @@
 package com.miniBOM.pojo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,22 +12,29 @@ import java.util.List;
 @Data
 public class Result <T> {
     private int code = 0;
+    @JsonProperty("result")
     private String msg;
+    @JsonProperty("data")
     public T data;
+    @JsonProperty("errors")
     public T error_data;
 
+    //错误返回code：1，msg：错误信息
     public static Result error(String msg){
         return new Result(1,msg,null,null);
     }
 
+    //错误返回msg和error_data
     public static <E> Result<E> error(String msg, E error_data){
         return new Result(1,msg,null,error_data);
     }
 
+    //无返回data
     public static Result success(){
         return new Result(0,"SUCCESS",null,null);
     }
 
+    //正常返回data
     public static <E> Result<E> success(E data){
         return new Result(0,"SUCCESS",data,null);
     }
