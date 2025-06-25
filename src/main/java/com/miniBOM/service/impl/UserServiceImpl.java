305818b5cb1bd933b909846761611f644e5566b0
RANN000 <1170883985@qq.com>
2025-06-25 10:54:36 +0800

    完整版

--- src/main/java/com/miniBOM/service/impl/UserServiceImpl.java
+++ src/main/java/com/miniBOM/service/impl/UserServiceImpl.java
@@ -13,17 +13,23 @@ import com.miniBOM.service.UserService;
 import com.miniBOM.utils.JwtUtil;
 import com.miniBOM.utils.Md5Util;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserDao userDao;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     @Override
     public User findByUsername(String name) {
@@ -33,9 +39,9 @@ public class UserServiceImpl implements UserService {
     // 用户注册
     @Override
     public Result registerUser(String username,
-                                String password,
-                                String phoneNumber,
-                                String email) {
+                               String password,
+                               String phoneNumber,
+                               String email) {
 
         //密码加密
         String psw = Md5Util.getMD5String(password);
@@ -59,8 +65,11 @@ public class UserServiceImpl implements UserService {
             claims.put("id", user.getId());
             claims.put("username", user.getName());
             String token = JwtUtil.genToken(claims);
+            //把token存储在redis中
+            ValueOperations<String, String> operations = stringRedisTemplate.opsForValue();
+            operations.set(token,token,12, TimeUnit.HOURS);
 
-            return Result.success("SUCCESS",token);
+            return Result.success("SUCCESS", token);
         }
 
         return Result.error("密码错误");
@@ -75,10 +84,10 @@ public class UserServiceImpl implements UserService {
     public Result updatePwd(String name, String old_pwd, String new_pwd) {
         User user = userDao.getUserByUserName(name);
         String psw1 = Md5Util.getMD5String(old_pwd);
-        if(!user.getPassword().equals(psw1)) {
+        if (!user.getPassword().equals(psw1)) {
             return Result.error("原密码输入错误");
         }
-        if(!new_pwd.matches("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+])\\S{8,32}$")){
+        if (!new_pwd.matches("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+])\\S{8,32}$")) {
             return Result.error("新密码输入格式错误");
         }
 
