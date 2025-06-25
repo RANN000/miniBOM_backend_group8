fad3481502c7680c126850ae122826f43d201a15
RANN000 <1170883985@qq.com>
2025-06-25 13:52:04 +0800

    redis相关说明

--- src/main/java/com/miniBOM/interceptors/LoginInterceptor.java
+++ src/main/java/com/miniBOM/interceptors/LoginInterceptor.java
@@ -1,48 +1,48 @@
-//package com.miniBOM.interceptors;
-//
-//import com.miniBOM.utils.JwtUtil;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-//import com.miniBOM.utils.ThreadLocalUtil;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.data.redis.core.StringRedisTemplate;
-//import org.springframework.data.redis.core.ValueOperations;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.servlet.HandlerInterceptor;
-//
-//import java.util.Map;
-//
-//@Component
-//public class LoginInterceptor implements HandlerInterceptor {
-//
-//    @Autowired
-//    private StringRedisTemplate stringRedisTemplate;
-//
-//    @Override
-//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-//        String token = request.getHeader("Authorization");
-//
-//        try {
-//            //redis
-//            ValueOperations<String, String> operations = stringRedisTemplate.opsForValue();
-//            String redisToken = operations.get(token);
-//            if (redisToken == null) {
-//                throw new RuntimeException();
-//            }
-//
-//            Map<String, Object> claims = JwtUtil.parseToken(token);
-//            ThreadLocalUtil.set(claims);
-//            return true;
-//        } catch (Exception e) {
-//            response.setStatus(401);
-//            return false;
-//        }
-//    }
-//
-//    @Override
-//    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-//        ThreadLocalUtil.remove();
-//    }
-//}
+package com.miniBOM.interceptors;
+
+import com.miniBOM.utils.JwtUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.miniBOM.utils.ThreadLocalUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.Map;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String token = request.getHeader("Authorization");
+
+        try {
+            //redis
+            ValueOperations<String, String> operations = stringRedisTemplate.opsForValue();
+            String redisToken = operations.get(token);
+            if (redisToken == null) {
+                throw new RuntimeException();
+            }
+
+            Map<String, Object> claims = JwtUtil.parseToken(token);
+            ThreadLocalUtil.set(claims);
+            return true;
+        } catch (Exception e) {
+            response.setStatus(401);
+            return false;
+        }
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        ThreadLocalUtil.remove();
+    }
+}
